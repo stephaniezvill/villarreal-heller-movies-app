2d8383b22f5a2d69d4f32986f5fc048abcccb8d9
Ben Heller <benheller1016@gmail.com>
2023-10-26 14:09:53 -0500

    API functions implemented

--- js/main.js
+++ js/main.js
@@ -2,16 +2,27 @@ import * as api from "./movies-api.js";
 
 "use strict";
 
-let movie;
-
 async function main() {
 
-    let movies = await api.getAllMovies();
-    // movie = api.getMovie(3);
-
-    console.log(movies);
+    // let newMovie = {
+    //     "title": "Future Movie",
+    //     "director": "Some Guy",
+    //     "released": 2121,
+    //     "rating": 3,
+    //     "genre": "Horror",
+    //     "movieSummary": "New Summary",
+    // }
+    // newMovie = await api.addMovie(newMovie);
+    // newMovie.genre = "Something else";
+    // newMovie = await api.editMovie(newMovie);
+    // let deletedMovie = await api.deleteMovie(12)
+//     let movies = await api.getAllMovies();
+// try {
+//     console.log(newMovie);
+//     console.log(movies);
+// } catch (error) {
+//     console.error(error);
+// }
 }
 
 main();
-
-// console.log(movie);
--- js/movies-api.js
+++ js/movies-api.js
@@ -6,34 +6,68 @@ const MOVIE_HOST = "http://localhost:3000";
 export async function getAllMovies() {
     try {
         return fetch(`${MOVIE_HOST}/movies`)
-            .then(response => response.json())
-            .then(movies => movies);
+            .then(response => response.json());
     } catch(error) {
         console.error (error);
     }
 }
 
-export async function getMovie(id) {
+// export async function getMovie(id) {
+//     try {
+//         movies = await getAllMovies();
+//         for (let movie of movies) {
+//             if (movie.id === id) {
+//                 return movie;
+//             }
+//         }
+//     } catch (error) {
+//         console.error(error);
+//     }
+// }
+
+export async function addMovie(movie) {
     try {
-        movies = await getAllMovies();
-        for (let movie of movies) {
-            if (movie.id === id) {
-                return movie;
-            }
+        const options = {
+            method: "POST",
+            headers: {
+                "Content-type": "application/json"
+            },
+            body: JSON.stringify(movie)
         }
-    } catch (error) {
+        return fetch(`${MOVIE_HOST}/movies`, options)
+            .then(response => response.json());
+    }
+    catch {
         console.error(error);
     }
 }
 
-export async function addMovie(name) {
-
+export async function editMovie(movie) {
+    try {
+        const options = {
+            method: "PUT",
+            headers: {
+                "Content-type": "application/json"
+            },
+            body: JSON.stringify(movie)
+        }
+        return fetch (`${MOVIE_HOST}/movies/${movie.id}`, options)
+            .then(response => response.json());
+    }
+    catch(error) {
+        console.error (error);
+    }
 }
 
-export async function editMovie(id) {
+export async function deleteMovie(id) {
+    try {
+        const options = {
+            method: "DELETE"
+        }
 
-}
-
-async function deleteMovie(id) {
-
+        return fetch(`${MOVIE_HOST}/movies/${id}`, options)
+            .then(response => response.json());
+    } catch (error) {
+        console.error (error);
+    }
 }
